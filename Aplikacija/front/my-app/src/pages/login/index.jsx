0530d8d8bd29b7c969027d9378f4953d207d41a7
MarkoMilosavljevic00 <marko.milosavljevicc@elfak.rs>
2022-05-17 19:04:13 +0200

    BACK - Povratak na moje od sinoc

--- Aplikacija/front/my-app/src/pages/login/index.jsx
+++ Aplikacija/front/my-app/src/pages/login/index.jsx
@@ -39,7 +39,7 @@ const Login = () => {
   }, [user, pwd]);
 
   const handleSubmit = async e => {
-    e.preventDefault();
+    //e.preventDefault();
 
     try {
       const response = await axios.post(
@@ -132,6 +132,7 @@ const Login = () => {
                 style={{ backgroundColor: 'green' }}
                 variant="contained"
                 color="success"
+                onClick={() => handleSubmit()}
               >
                 Uloguj se
               </Button>
